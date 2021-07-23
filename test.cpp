361f72338b05b2309ca193089bd02de5746647f7
Mahadi <mahadi.xlr8@gmail.com>
2021-07-24 00:07:57 +0600

    even odd check program

--- test.cpp
+++ test.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<endl;
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if (a%2)
+    {
+        cout<<"the number is odd\n";
+        /* code */
+    }
+    else if(a==0){
+        cout<<"the number is zero\n";
+    }
+    else cout<<"the number is even\n";
+    
 
     return 0;
 }
